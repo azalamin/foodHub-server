96358e758c5c6f6bdd6b1f65e86fcaceb202a8ba
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-28 22:04:53 +0600

    user model added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
+import { toNodeHandler } from "better-auth/node";
 import express, { Application } from "express";
 
 import cors from "cors";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
